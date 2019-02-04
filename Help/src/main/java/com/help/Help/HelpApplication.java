e91567fb4d773b510376fde33a9e0f051bd98c5d
dilip.duraiswamy <dilip.duraiswamy@BLR-L1718-01692.actcorp.local>
2019-02-04 22:22:57 +0530

    Initial Project Commit
    Change-Id: I64ce8fcce7910352761b103aeda100ac6a689132
    Change-Id: I00000000000001
    Signed-off-by: dilip.duraiswamy <dilip.duraiswamy@BLR-L1718-01692.actcorp.local>

---
+++ Help/src/main/java/com/help/Help/HelpApplication.java
@@ -0,0 +1,14 @@
+package com.help.Help;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpApplication.class, args);
+	}
+
+}
+
