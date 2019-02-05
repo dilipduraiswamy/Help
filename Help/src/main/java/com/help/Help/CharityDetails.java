16d98976740f99759a277bd1541fdceb3d3804ee
dilip.duraiswamy <dilip.duraiswamy@BLR-L1718-01692.actcorp.local>
2019-02-05 20:52:28 +0530

    Request Pojo for charity

---
+++ Help/src/main/java/com/help/Help/CharityDetails.java
@@ -0,0 +1,84 @@
+package com.help.Help;
+
+import java.util.ArrayList;
+
+public class CharityDetails {
+
+	private String name;
+	
+	private String phoneNumber;
+	
+	private String address;
+	
+	private ArrayList<String> images;
+	
+	private ArrayList<String> videos;
+	
+	private String description;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public ArrayList<String> getImages() {
+		return images;
+	}
+
+	public void setImages(ArrayList<String> images) {
+		this.images = images;
+	}
+
+	public ArrayList<String> getVideos() {
+		return videos;
+	}
+
+	public void setVideos(ArrayList<String> videos) {
+		this.videos = videos;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public CharityDetails(String name, String phoneNumber, String address, ArrayList<String> images,
+			ArrayList<String> videos, String description) {
+		super();
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+		this.address = address;
+		this.images = images;
+		this.videos = videos;
+		this.description = description;
+	}
+
+	public CharityDetails() {
+		super();
+	}
+	
+	
+	
+}
